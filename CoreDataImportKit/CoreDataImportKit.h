0b777f4ff825435c20890aec0268c72c321e54b8
Ryan Mathews <ryan@ryanjm.com>
2016-02-09 17:10:52 -0800

    Initial Commit

---
+++ CoreDataImportKit/CoreDataImportKit.h
@@ -0,0 +1,19 @@
+//
+//  CoreDataImportKit.h
+//  CoreDataImportKit
+//
+//  Created by Ryan Mathews on 2/9/16.
+//  Copyright © 2016 OrangeQC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreDataImportKit.
+FOUNDATION_EXPORT double CoreDataImportKitVersionNumber;
+
+//! Project version string for CoreDataImportKit.
+FOUNDATION_EXPORT const unsigned char CoreDataImportKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDataImportKit/PublicHeader.h>
+
+
